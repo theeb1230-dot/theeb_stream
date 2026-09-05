a4757b7e07f1e2d28961ba599f8a8d4ca8aba6c2
Arab_Theeb <theeb1230@gmail.com>
2026-09-05 22:37:09 +0300

    fix: update Dart package imports after rebrand

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:maxstream/main.dart';
+import 'package:theeb_stream/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
