fa1b84b3eed8eed8fe8513c9d2a7ab0877087727
Arab_Theeb <theeb1230@gmail.com>
2026-09-06 03:25:22 +0300

    test: keep startup errors redacted

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:theeb_stream/main.dart';
 
 void main() {
-  testWidgets('Arabic startup error surface is renderable', (tester) async {
+  testWidgets('Arabic startup error surface is renderable and redacted', (tester) async {
     await tester.pumpWidget(const ErrorApp(error: 'اختبار'));
 
     expect(find.textContaining('تعذر تشغيل التطبيق'), findsOneWidget);
-    expect(find.textContaining('اختبار'), findsOneWidget);
+    expect(find.textContaining('اختبار'), findsNothing);
   });
 }
