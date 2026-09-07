dcd29ad24b49e11f842b225bfc6f20744a622a70
Arab_Theeb <theeb1230@gmail.com>
2026-09-08 01:04:06 +0300

    Align TV version metadata with 1.6.5+13

--- android/tvapp/build.gradle.kts
+++ android/tvapp/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.theebstream.tv"
         minSdk = 23
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.6.4"
+        versionCode = 13
+        versionName = "1.6.5"
         vectorDrawables.useSupportLibrary = true
     }
 
