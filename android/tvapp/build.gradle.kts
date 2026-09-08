0b39c5581838d491b3979c2a8800ad9a46a8f276
Arab_Theeb <theeb1230@gmail.com>
2026-09-08 03:00:47 +0300

    release(tv): align version with 2.0.0+14

--- android/tvapp/build.gradle.kts
+++ android/tvapp/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.theebstream.tv"
         minSdk = 23
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.6.5"
+        versionCode = 14
+        versionName = "2.0.0"
         vectorDrawables.useSupportLibrary = true
     }
 
